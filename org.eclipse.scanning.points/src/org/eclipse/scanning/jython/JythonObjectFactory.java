2b9d47036530cfa556d49dd3f227f72b096c25c2
Matthew Gerring <matthew.gerring@gmail.com>
2017-03-08 14:12:18 +0000

    Used generics, better when reusing factory elsewhere.

--- org.eclipse.scanning.points/src/org/eclipse/scanning/jython/JythonObjectFactory.java
+++ org.eclipse.scanning.points/src/org/eclipse/scanning/jython/JythonObjectFactory.java
@@ -5,16 +5,16 @@ import org.python.core.PyObject;
 import org.python.core.PySystemState;
 
 // This class creates Java objects from Jython classes
-public final class JythonObjectFactory {
+public final class JythonObjectFactory<T> {
 
 
-	private final Class<?> javaClass;
+	private final Class<T> javaClass;
 	private final PyObject pyClass;
 
 	// This constructor passes through to the other constructor with the SystemState
-	public JythonObjectFactory(Class<?> javaClass, String moduleName, String className) {
+	public JythonObjectFactory(Class<T> javaClass, String moduleName, String className, String... bundleNames) {
 
-		JythonInterpreterManager.setupSystemState();
+		JythonInterpreterManager.setupSystemState(bundleNames);
 		PySystemState state = Py.getSystemState();
 
 		this.javaClass = javaClass;
@@ -26,26 +26,31 @@ public final class JythonObjectFactory {
 	// The following methods return a coerced Jython object based upon the pieces of
 	// information that were passed into the factory, for various argument structures
 
-	public Object createObject() {
-		return pyClass.__call__().__tojava__(javaClass);
+	@SuppressWarnings("unchecked")
+	public T createObject() {
+		return (T)pyClass.__call__().__tojava__(javaClass);
 	}
-	public Object createObject(Object arg1) {
-		return pyClass.__call__(Py.java2py(arg1)).__tojava__(javaClass);
+	@SuppressWarnings("unchecked")
+	public T createObject(Object arg1) {
+		return (T)pyClass.__call__(Py.java2py(arg1)).__tojava__(javaClass);
 	}
-	public Object createObject(Object arg1, Object arg2) {
-		return pyClass.__call__(Py.java2py(arg1), Py.java2py(arg2)).__tojava__(javaClass);
+	@SuppressWarnings("unchecked")
+	public T createObject(Object arg1, Object arg2) {
+		return (T)pyClass.__call__(Py.java2py(arg1), Py.java2py(arg2)).__tojava__(javaClass);
 	}
-	public Object createObject(Object arg1, Object arg2, Object arg3) {
-		return pyClass.__call__(Py.java2py(arg1), Py.java2py(arg2), Py.java2py(arg3)).__tojava__(javaClass);
+	@SuppressWarnings("unchecked")
+	public T createObject(Object arg1, Object arg2, Object arg3) {
+		return (T)pyClass.__call__(Py.java2py(arg1), Py.java2py(arg2), Py.java2py(arg3)).__tojava__(javaClass);
 	}
-	public Object createObject(Object args[], String keywords[]) {
+	@SuppressWarnings("unchecked")
+	public T createObject(Object args[], String keywords[]) {
 		PyObject convertedArgs[] = new PyObject[args.length];
 		for (int i = 0; i < args.length; i++) {
 			convertedArgs[i] = Py.java2py(args[i]);
 		}
-		return pyClass.__call__(convertedArgs, keywords).__tojava__(javaClass);
+		return (T)pyClass.__call__(convertedArgs, keywords).__tojava__(javaClass);
 	}
-	public Object createObject(Object... args) {
+	public T createObject(Object... args) {
 		return createObject(args, Py.NoKeywords);
 	}
 
