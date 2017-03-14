92318f0a8e40d843819d68e3c893cc0e147bbfe7
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-03-14 14:26:00 +0000

    Refactor ModelArgument classes
    Updates model argument classes to reflect changes to API. Now we pass in
    an IQueueValue instead of a concrete type to the QueueTableVariable - 
    this makes it possible to stack IQueueVariables and maintain type 
    safety.
    
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

---
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/queues/models/arguments/QueueValue.java
@@ -0,0 +1,42 @@
+package org.eclipse.scanning.api.event.queues.models.arguments;
+
+import org.eclipse.scanning.api.event.queues.models.QueueModelException;
+
+/**
+ * Basic model argument which holds a single object. Typically this would be a 
+ * String, Double, Integer or another simple type, but could also be a List, 
+ * Map or other collection. 
+ * 
+ * @author Michael Wharmby
+ *
+ * @param <V> Type of the value held by this argument.
+ */
+public class QueueValue<V> implements IQueueValue<V> {
+	
+	private V value;
+	
+	/**
+	 * Construct a new QueueValue with a given value.
+	 * 
+	 * @param parameter value V to be stored in this argument.
+	 */
+	public QueueValue(V value) {
+		this.value = value;
+	}
+
+	@Override
+	public V evaluate() {
+		if (value == null) throw new QueueModelException("Value was never initialised");
+		return value;
+	}
+	
+	/**
+	 * Change the value of this QueueValue.
+	 * 
+	 * @param value V of this QueueValue.
+	 */
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+}
