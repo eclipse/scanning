56204f4d8e0af678eed5ee0e8988bf695580f24d
Matthew Gerring <matthew.gerring@gmail.com>
2017-05-16 11:07:40 +0100

    Removed Jackson dependency as dependencies are not allowed on API.

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/scan/DeviceValueMultiPosition.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/event/scan/DeviceValueMultiPosition.java
@@ -23,14 +23,12 @@ import java.util.Map;
 
 import org.eclipse.scanning.api.scan.ScannableValueTextProvider;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * This is a special Position class to allow multiple values to be returned from a scannable. It is a temporary
  * creation until we understand why can't use a standard List or Map for a DeviceValue in a DeviceRequest. 
  */
 public class DeviceValueMultiPosition implements ScannableValueTextProvider {
-	@JsonProperty("values")
 	private Map<String, Double> values = new LinkedHashMap<>();
 
 	public Double get(String parameter) {
