955460e914381ccbe3b3745047bd4a0c3acd3a7e
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2017-09-14 10:14:23 +0100

    [DAQ-504] MalcolmTable's iterator should throw NoSuchElementException
    rather than an ArrayIndexOutOfBoundsException if next() is called after
    the last row.

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/malcolm/MalcolmTable.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/malcolm/MalcolmTable.java
@@ -17,26 +17,27 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 /**
  * Class representing a table in Malcolm Format (List of columns with data).
  * Instances of this class are NOT thread safe.
  */
 public class MalcolmTable implements Iterable<Map<String, Object>> {
-	
+
 	private Map<String, List<Object>> tableData;
 	private Map<String, Class<?>> tableDataTypes;
 	private List<String> headings;
 	private int numRows;
-	
+
 	public MalcolmTable() {
-		
+
 	}
-	
+
 	/**
 	 * Create a new Malcolm table with the given columns data and data types. The
 	 * size of each column must be equal, and the keys of both maps must be the same, these
-	 * are the column headings.  
+	 * are the column headings.
 	 * @param tableAsMap a list of values for each column, of equal lengths
 	 * @param dataTypes a map of the data types for each column, this map must have the
 	 *    same keys as the tableAsMap
@@ -46,25 +47,25 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 		if (tableAsMap.size() != dataTypes.size()) {
 			throw new IllegalArgumentException("The given arguments are not of the same size");
 		}
-		
+
 		tableData = tableAsMap;
-		
+
 		numRows = tableData.isEmpty() ? 0 : tableData.values().iterator().next().size();
 		tableDataTypes = dataTypes;
 		headings = new LinkedList<String>(tableAsMap.keySet());
-		
+
 		for (String heading : headings) {
 			if (!dataTypes.containsKey(heading)) {
 				throw new IllegalArgumentException("The types map has no entry for column " + heading);
 			}
-			
+
 			if (tableAsMap.get(heading).size() != numRows) {
 				throw new IllegalArgumentException(String.format("The column %s has size %d, should be %d",
 						heading, tableAsMap.get(heading).size(), numRows));
 			}
 		}
 	}
-	
+
 	/**
 	 * Creates a new empty table with columns of the given types.
 	 * @param dataTypes map from column name to data type for that column
@@ -72,14 +73,14 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 	public MalcolmTable(Map<String, Class<?>> dataTypes) {
 		tableDataTypes = dataTypes;
 		headings = new LinkedList<String>(tableDataTypes.keySet());
-		
+
 		tableData = new LinkedHashMap<>(headings.size());
 		for (String heading : headings) {
 			tableData.put(heading, new ArrayList<>());
 		}
 		numRows = 0;
 	}
-	
+
 	public List<Object> getColumn(String columnName) {
 		List<Object> column = tableData.get(columnName);
 		if (column != null) {
@@ -87,7 +88,7 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 		}
 		throw new RuntimeException("Unknown column: " + columnName);
 	}
-	
+
 	public Class<?> getColumnClass(String columnName) {
 		List<Object> column = tableData.get(columnName);
 		if (column != null) {
@@ -95,28 +96,28 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 		}
 		throw new RuntimeException("Unknown column: " + columnName);
 	}
-	
+
 	public Map<String, Object> getRow(int rowIndex) {
 		if (rowIndex >= numRows) {
 			throw new IndexOutOfBoundsException("No such row " + rowIndex + ", number of rows = " + numRows);
 		}
-		
+
 		Map<String, Object> row = new LinkedHashMap<>(headings.size());
 		for (String heading : headings) {
 			row.put(heading, getCellValue(heading, rowIndex));
 		}
-		
+
 		return row;
 	}
-	
+
 	public Object getCellValue(String columnName, int rowIndex) {
 		return getColumn(columnName).get(rowIndex);
 	}
-	
+
 	public List<String> getHeadings() {
 		return headings;
 	}
-	
+
 	public void addRow(Map<String, Object> newRow) {
 		if (newRow.size() != headings.size()) {
 			throw new IllegalArgumentException("The size of the map for the new row must match the number of columns in the table.");
@@ -135,11 +136,11 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 	public Iterator<Map<String, Object>> iterator() {
 		return new MalcolmTableRowIterator();
 	}
-	
+
 	public class MalcolmTableRowIterator implements Iterator<Map<String, Object>> {
-		
+
 		private int rowNum = 0;
-		
+
 		@Override
 		public boolean hasNext() {
 			return rowNum < numRows;
@@ -147,9 +148,12 @@ public class MalcolmTable implements Iterable<Map<String, Object>> {
 
 		@Override
 		public Map<String, Object> next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
 			return getRow(rowNum++);
 		}
-		
+
 	}
 
 	public Map<String, List<Object>> getTableData() {
